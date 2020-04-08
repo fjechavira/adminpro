8bd6a6b60d908c93d3be56c951d0a343558c5058
Emanuel Chavira <echavira@grupoesquimal.mx>
2020-04-08 15:08:40 -0500

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { ShareModule } from '../share/share.module';
+import { PAGES_ROUTES } from './pages.routes';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+
+
+
+
+@NgModule({
+    declarations : [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        ShareModule,
+        PAGES_ROUTES
+    ]
+})
+export class PagesModule { }
